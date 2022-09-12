28ccffb16bcf99a8106daee0e2bda1462079f16e
Rohit Bahuguna <rohitbahuguna.4446@gmail.com>
2022-09-12 08:36:27 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-
+const router = require('./routes')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use('/' , router)
 module.exports = app;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 
-router.get('/:name/:age', (req, res, next) => {
+router.get('/:name/:age', async (req, res, next) => {
+    const { name , age } = req.params
+    res.status(200).json({
+        message: `Hello ${name}, you are ${age} years old`
+    })
 })
 module.exports = router;
